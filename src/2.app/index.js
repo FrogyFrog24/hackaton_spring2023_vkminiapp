7dc4a5175b4782e7c6e2e2f1f6a49c98e00554c3
FrogyFrog24 <gusar.nikita6@gmail.com>
2023-03-18 03:29:31 +0300

    add theme toggle

--- src/2.app/index.js
+++ src/2.app/index.js
@@ -2,13 +2,34 @@ import { ChooseUniversityPage } from '3.pages/ChooseUniversityPage';
 import { ScheduleChoosePage } from '3.pages/ScheduleChoosePage';
 import { PanelTypes, ViewTypes } from '6.entities/structure';
 import { AdaptivityProvider, AppRoot, ConfigProvider, Panel, Root, View } from '@vkontakte/vkui';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouterSelector, useRouterActions } from 'react-router-vkminiapps';
+import bridge from "@vkontakte/vk-bridge";
 
 const App = () => {
   const { activeView, activePanel } = useRouterSelector();
   const { toView, toPanel, toBack } = useRouterActions();
-  const [appearance, setAppereance] = useState("dark")
+  const [appearance, setAppereance] = useState()
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(async () => {
+    await bridge.send('VKWebAppGetConfigResult')
+      .then((data) => {
+        if (data) {
+          setAppereance(data.appearance)
+          setLoading(false)
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [])
+
+  bridge.subscribe((e) => {
+    if (e.detail.type === 'VKWebAppUpdateConfig') {
+      setAppereance(e.detail.data.appearance)
+    }
+  });
 
   return (
     <ConfigProvider isWebView={true} appearance={appearance}>
